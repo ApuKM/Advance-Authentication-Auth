c7b8496b1b0c363ae19db92b48a3641171d18e85
ApuKM <apukumar180@gmail.com>
2026-01-03 22:41:11 +0600

    Setting up auth and prisma

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <div>LogInPage</div>
+  )
+}
+
+export default page
---
+++ app/protected/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <div className="bg-red-300 h-screen flex items-center justify-center text-6xl">Protected Page</div>
+  )
+}
+
+export default page
